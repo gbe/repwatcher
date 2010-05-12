fb6d8f78f765757c56da1c1523a0d4caf71013c8
Gregory BELLIER <gregory.bellier@gmail.com>
2010-05-12 15:39:10 +0200

    [RW2]	License
    	- Added the license in the source code and the folders
    	- Deleted server and client files, the ones without SL

--- RW2/rw_server/main.ml
+++ RW2/rw_server/main.ml
@@ -1,7 +1,27 @@
+(*
+    This file is part of Repwatcher.
+
+    Repwatcher is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Repwatcher is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Repwatcher; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*)
+
+
+
 (* TO DO
 
-- Ajouter le SQL
-- Voir pour mettre en surveillance des fichiers et pas seulement des dossiers (cf fichier de config donc)
+- Voir pour mettre en surveillance des fichiers et pas seulement des dossiers
 - Dans report.ml, revoir le système des erreurs. Mettre des numéros d'erreur et appeler directement dans Report les erreurs. Enlever les add_report depuis Core.ml
 *)
 
