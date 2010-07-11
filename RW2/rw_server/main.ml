a0f092e5f54178bf3b6a28c2584e420e1216f5f0
Gregory Bellier <gregory.bellier@gmail.com>
2010-07-11 19:51:12 +0200

    [RW2] 	- Move the pipe loop from main to pipe_listening.ml (new file)

--- RW2/rw_server/main.ml
+++ RW2/rw_server/main.ml
@@ -133,34 +133,6 @@ let init () =
 
 
 
-let wait_pipe_from_child_process () =
-  let bufsize = 1024 in
-  let buf = String.create bufsize in
-  
-  while true do
-    let recv = Unix.read Pipe.tor2 buf 0 bufsize in
-    Printf.printf "Main, tor2 franchit. recv= %d\n" recv;
-    if recv > 0 then
-      begin
-	match String.sub buf 0 recv with
-	| "ask_current_dls" ->
-
-	    let l_current_dls = Hashtbl.fold (fun (_,file) date ret -> ret@[(file.f_login, (Txt_operations.escape_for_notify file.f_name), date)] ) Files_progress.ht [] in
-	    
-	    (* We go through the pipe only if it's necessary *)
-	    if List.length l_current_dls > 0 then
-	      begin
-		let str_current_dls = Marshal.to_string (Old_notif l_current_dls) [Marshal.No_sharing] in
-		ignore (Unix.write Pipe.tow str_current_dls 0 (String.length str_current_dls))
-	      end
-	| _ -> Report.report (Log ("Err. The server received an unknown command", Level_2))
-      end;
-    Pervasives.flush Pervasives.stdout
-  done
-;;
-
-
-
 
 (* Fonction main *)
 let _ =
@@ -194,7 +166,7 @@ let _ =
       | _ ->
 	  begin
 	  if conf.c_notify_rem then begin
-	    ignore (Thread.create wait_pipe_from_child_process ())
+	    ignore (Thread.create Pipe_listening.wait_pipe_from_child_process ())
 	  end;
 	    
 	    Core.print_ht ();
