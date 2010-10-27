669e976b4c301d6fb4a85ff7de0fd95386e985bb
Grégory BELLIER <gregory.bellier@gmail.com>
2010-10-27 15:45:12 +0200

    Ajout du fichier list_dirs.ml qui comprend la fonction qui doit venir écraser l'appel système à 'find'

---
+++ list_dirs.ml
@@ -0,0 +1,55 @@
+open Unix;;
+
+let root = "/home/dest/Musique";;
+
+let rec list_dirs dir = 
+
+  let l = ref [] in
+  let dh_opt = ref None in
+
+
+    (try
+       (* Teste si le répertoire donné en argument existe *)
+       match Sys.is_directory dir with
+	 | true ->
+	     dh_opt := Some (opendir dir);
+	     l := dir::(!l)
+	 | false -> ()
+     with Sys_error _ -> ()
+    );
+    
+    
+    match !dh_opt with
+      | None -> []
+      | Some dh ->
+	  begin
+	    try
+	      while true do
+		let entry = readdir dh in
+		  try
+		    let fullpath = (dir^"/"^entry) in
+		    (* Teste si c'est un fichier ou un dossier *)
+		    match Sys.is_directory fullpath with
+		      | true ->
+			  begin
+			    match entry with
+			      | ("." | "..") -> ()
+			      | _            -> 
+				  l := List.rev((list_dirs fullpath))@(!l)
+			  end
+		      | false -> ()
+		  with Sys_error err -> print_endline err
+		    
+	      done;
+	    with End_of_file -> closedir dh
+	  end;
+	  List.rev !l
+;;
+
+
+
+let _ =
+  let l = list_dirs root in
+  List.iter (fun d -> print_endline d) l;
+  Pervasives.flush Pervasives.stdout
+;;
