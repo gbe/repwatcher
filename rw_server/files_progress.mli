399377074870689e5466bd8bb2d0347e2fb8a85c
Grégory BELLIER <gregory.bellier@gmail.com>
2011-05-12 22:43:25 +0200

    swap two lines

--- rw_server/files_progress.mli
+++ rw_server/files_progress.mli
@@ -1,2 +1,2 @@
-val ht : (Inotify.wd * Types.f_file, (string * int64)) Hashtbl.t
 (* The string here is the date and int64 the offset when the file has been accessed *)
+val ht : (Inotify.wd * Types.f_file, (string * int64)) Hashtbl.t
