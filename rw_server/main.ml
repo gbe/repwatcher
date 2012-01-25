a5331ff483ae0578b0d78958bf746c15dd62e79f
Grégory BELLIER <gregory.bellier@gmail.com>
2012-01-25 14:41:23 +0100

    Moved try/with block to avoid silent errors

--- rw_server/main.ml
+++ rw_server/main.ml
@@ -213,10 +213,13 @@ This is free software under the MIT license.\n\n";
       (* **************************** *)
 
       while !loop do
-	try
-	  let _,_,_ = Unix.select [ Core.fd ] [] [] (-1.) in
-	  let event_l = Inotify.read Core.fd in
-	  List.iter Events.what_to_do event_l
-	with Unix_error (_,_,_) -> () (* Unix.select triggers this error when ctrl+c is pressed *)
+	begin
+	  try
+	    ignore (Unix.select [ Core.fd ] [] [] (-1.));
+	  with Unix_error (_,_,_) -> () (* Unix.select triggers this error when ctrl+c is pressed *)
+	end;
+	let event_l = Inotify.read Core.fd in
+	List.iter Events.what_to_do event_l
+
       done;
 ;;
