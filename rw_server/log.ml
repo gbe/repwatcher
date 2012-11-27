4d7c47990a47b9e35e19ce5149e42da8664215e9
Grégory BELLIER <gregory.bellier@gmail.com>
2012-11-27 12:00:12 +0100

    log: more precise try/with + object

--- rw_server/log.ml
+++ rw_server/log.ml
@@ -2,37 +2,66 @@ open Syslog
 open Types
 open Types_conf
 
+exception Cid_error
+
+class syslog txt log_level =
+object(self)
+  val mutable syslog_cid = None
+
+  method private _get_cid=
+    match syslog_cid with
+      | None -> raise Cid_error
+      | Some cid' -> cid'
+
+  method openlog_connect =
+    try
+      syslog_cid <-
+	Some (openlog ~facility:`LOG_USER ~flags:[] "rw_server");
+    with _ -> prerr_endline "Syslog error: openlog"
+
+  method private _log syslog_level txt =
+    try
+      syslog self#_get_cid syslog_level txt
+    with 
+      | Cid_error -> prerr_endline "Syslog error: sendlog cid error"
+      | _ -> prerr_endline "Syslog error: sendlog"
+
+
+  method sendlog =
+    let conf = Config.get () in
+
+    match conf.c_log with
+      | Disabled -> () (* don't log *)
+      | Regular  ->
+	begin
+	  match log_level with
+	    | Normal       -> self#_log `LOG_INFO txt
+	    | Normal_Extra -> () (* don't log *)
+	    | Error        ->
+	      prerr_endline txt;
+	      self#_log `LOG_ERR txt
+	end
+      | Debug ->
+	begin
+	  match log_level with
+	    | (Normal | Normal_Extra) -> self#_log `LOG_DEBUG txt
+	    | Error ->
+	      prerr_endline txt;
+	      self#_log `LOG_ERR txt
+	end
+
+  method closelog =
+    try
+      closelog self#_get_cid
+    with
+      | Cid_error -> prerr_endline "Syslog error: closelog cid error"
+      | _ -> prerr_endline "Syslog error: closelog"
+
+end;;
+
 let log (txt, log_level) =
-
-  let conf = Config.get () in
-
-  try
-    let cid =
-      openlog ~facility:`LOG_USER ~flags:[] "rw_server"
-    in
-
-    begin
-      match conf.c_log with
-	| Disabled -> () (* don't log *)
-	| Regular  ->
-	  begin
-	    match log_level with
-	      | Normal       -> syslog cid `LOG_INFO txt
-	      | Normal_Extra -> () (* don't log *)
-	      | Error        ->
-		prerr_endline txt;
-		syslog cid `LOG_ERR txt
-	  end
-	| Debug ->
-	  begin
-	    match log_level with
-	      | (Normal | Normal_Extra) -> syslog cid `LOG_DEBUG txt
-	      | Error ->
-		prerr_endline txt;
-		syslog cid `LOG_ERR txt
-	  end;
-    end;
-    closelog cid;
-
-  with _ -> prerr_endline "Syslog error"
+  let sysl = new syslog txt log_level in
+  sysl#openlog_connect;
+  sysl#sendlog;
+  sysl#closelog
 ;;
