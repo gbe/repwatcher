884feda450d1437da54a7f48199561da1b1d3afb
Grégory BELLIER <gregory.bellier@gmail.com>
2013-11-06 21:59:12 +0100

    update the first_known_offset when closing the file without having read the offset through the offset_thread

--- rw_server/sqldb.ml
+++ rw_server/sqldb.ml
@@ -11,6 +11,7 @@ object(self)
       | MySQL -> Mysql (new Mysqldb.mysqldb)
       | PostgreSQL -> Postgresql (new Postgresqldb.pgsql)
 
+  val mutable is_first_known_offset_set = false
 
   method is_connected =
     match connector with
@@ -42,19 +43,37 @@ object(self)
       | Postgresql con ->
 	con#file_opened f s_created creation_date filesize
 
+
   method file_closed closing_date filesize offset =
+
+    (* it happens that a file can be closed before the first offset check in
+     * the offset_thread resulting with the LAST_KNOWN_OFFSET set in RDBMS
+     * but not the first offset 
+     * Must be done before the file_close query as it disconnects the connection.
+     *)
+    if is_first_known_offset_set = false then
+      self#first_known_offset offset;
+
     match connector with
       | Mysql con ->
 	con#file_closed closing_date filesize offset
       | Postgresql con ->
 	con#file_closed closing_date filesize offset
 
+
+  (* the first known offset is necessarily
+   * the last known offset as well *)
   method first_known_offset offset =
-    match connector with
+    begin match connector with
       | Mysql con ->
-	con#first_known_offset offset
+	con#first_known_offset offset;
+	con#last_known_offset offset
       | Postgresql con ->
-	con#first_known_offset offset
+	con#first_known_offset offset;
+	con#last_known_offset offset
+    end;
+    (* var used in file_closed *)
+    is_first_known_offset_set <- true
 
   method last_known_offset offset =
     match connector with
